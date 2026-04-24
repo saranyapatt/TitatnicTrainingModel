f918ce41b424121fc158e70a6b3da197ca56e0ea
saranyapatt <213416715+saranyapatt@users.noreply.github.com>
2026-04-24 17:23:05 +0700

    Update project before optimization

--- exploringUnsupervised1.py
+++ exploringUnsupervised1.py
@@ -1,15 +1,124 @@
+import itertools
 import kagglehub
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import plot_confusion_matrix
+import seaborn as sns
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
-kagglehub.login()
+# kagglehub.login()
 # Download latest version
 # path = kagglehub.competition_download('titanic')
 # new_path = os.path.join(path + "/Users/mix/.cache/kagglehub/competitions/titanic/train.csv")
 new_path = os.path.join("/Users/mix/.cache/kagglehub/competitions/titanic/train.csv")
-print("Path to dataset files:", new_path)
+test_path = os.path.join("/Users/mix/.cache/kagglehub/competitions/titanic/test.csv")
 
+data_ori = pd.read_csv(new_path)
 data = pd.read_csv(new_path)
+data_test_ori = pd.read_csv(test_path)
+data_test = pd.read_csv(test_path)
+
+data['title'] = data['Name'].str.extract(',\\s*([A-Za-z]+)\.', expand=False)
+data_test['title'] = data_test['Name'].str.extract(',\\s*([A-Za-z]+)\.', expand=False)
+
+title_mapping = {
+    "Mr": "Mr",
+    "Miss": "Miss_Mrs", "Mrs": "Miss_Mrs", "Mlle": "Miss_Mrs", "Ms": "Miss_Mrs", "Mme": "Miss_Mrs",
+    "Master": "Master",
+    "Lady": "Royalty", "Sir": "Royalty", "Jonkheer": "Royalty", "Don": "Royalty", "Countess": "Royalty",
+    "Dr": "Officer", "Rev": "Officer", "Col": "Officer", "Major": "Officer", "Capt": "Officer"
+}
+
+data['title_group'] = data['title'].map(title_mapping)
+data_test['title_group'] = data['title'].map(title_mapping)
+
+title_dummies = pd.get_dummies(data['title_group'], prefix='Title', dtype=int)
+title_dummies_test = pd.get_dummies(data_test['title_group'], prefix='Title', dtype=int)
+
+data = pd.concat([data, title_dummies], axis=1)
+data_test = pd.concat([data_test, title_dummies_test], axis=1)
+
+data = data.drop(['Name', 'title', 'title_group'], axis=1)
+data_test = data_test.drop(['Name', 'title', 'title_group'], axis=1)
 
 
+data['Sex'] = data['Sex'].map({'male': 0, 'female': 1})
+data_test['Sex'] = data_test['Sex'].map({'male': 0, 'female': 1})
+
+data['Embarked'] = data['Embarked'].fillna(data['Embarked'].mode()[0])
+data_test['Embarked'] = data_test['Embarked'].fillna(data_test['Embarked'].mode()[0])
+
+embarked_dummies = pd.get_dummies(data['Embarked'], prefix='Embarked', dtype=int)
+embarked_dummies_test = pd.get_dummies(data_test['Embarked'], prefix='Embarked', dtype=int)
+
+data = pd.concat([data, embarked_dummies], axis=1)
+data_test = pd.concat([data_test, embarked_dummies_test], axis=1)
+data.drop('Embarked', axis=1, inplace=True)
+data_test.drop('Embarked', axis=1, inplace=True)
+
+titles = ['Title_Mr', 'Title_Miss_Mrs', 'Title_Master', 'Title_Royalty', 'Title_Officer']
+for title in titles:
+    median_val = data[data[title] == 1]['Age'].median()
+    data.loc[(data['Age'].isnull()) & (data[title] == 1), 'Age'] = median_val
+
+data['Age'] = data['Age'].fillna(data['Age'].median())
+
+titles_test = ['Title_Mr', 'Title_Miss_Mrs', 'Title_Master', 'Title_Officer']
+for title in titles_test:
+    median_val = data_test[data_test[title] == 1]['Age'].median()
+    data_test.loc[(data_test['Age'].isnull()) & (data_test[title] == 1), 'Age'] = median_val
+
+    median_val_fare = data_test[data_test[title] == 1]['Fare'].median()
+    data_test.loc[(data_test['Fare'].isnull()) & (data_test[title] == 1), 'Fare'] = median_val_fare
+data_test['Age'] = data_test['Age'].fillna(data_test['Age'].median())
+data_test['Fare'] = data_test['Fare'].fillna(data_test['Fare'].median())
+
+data.drop('Cabin', axis=1, inplace=True)
+data_test.drop('Cabin', axis=1, inplace=True)
+
+data.drop('Ticket', axis=1, inplace=True)
+data_test.drop('Ticket', axis=1, inplace=True)
+
+data.drop('PassengerId', axis=1, inplace=True)
+data_test.drop('PassengerId', axis=1, inplace=True)
+
+data_y = data['Survived']
+# data_y_test = data_test['Survived']
+data.drop('Survived', axis=1, inplace=True)
+# data_test.drop('Survived', axis=1, inplace=True)
+
+# scaler = StandardScaler()
+#
+# scaler.fit(data)
+# X_train = scaler.transform(data)
+# X_test = scaler.transform(data_test)
+#
+# from sklearn.neighbors import KNeighborsClassifier
+# classifier = KNeighborsClassifier(n_neighbors=3)
+# classifier.fit(X_train, data_y)
+# y_pred = classifier.predict(X_test)
+
+X_train, X_test, y_train, y_test = train_test_split(data, data_y, test_size=0.15, random_state=42)
+scaler = StandardScaler()
+
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Not Survived', 'Survived'])
+disp.plot(cmap='Blues')
+plt.title('Confusion Matrix on Validation Set')
+plt.show()
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
